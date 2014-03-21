750e436c7c145e04216d3f0e6c1d7dffe6a07ed9
berryman <alexbman@gmail.com>
2014-03-21 12:40:23 -0400

    test complete static flow table

--- ext/osu.py
+++ ext/osu.py
@@ -5,6 +5,8 @@ This module contains some flows for The Ohio State University DMZ
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpid_to_str
+from pox.lib.addresses import IPAddr, EthAddr
+import pox.lib.packet as pkt
 
 log = core.getLogger()
 
@@ -19,17 +21,63 @@ class DMZFlows(object):
         connection.addListeners(self)
 
         #Static flows
+        #Drop PVST+ traffic
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_NONE),
+                                             priority=9999,
+                                             match=of.ofp_match(in_port=64, nw_dst=EthAddr("01:00:0c:cc:cc:cd"))))
+
+        #Clemson Inbound to Controller
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_CONTROLLER),
+                                             priority=800,
+                                             match=of.ofp_match(in_port=64, dl_vlan=3070)))
+        #OSU DTN traffic outbound to Clemson to controller
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=20),
+                                             priority=800,
+                                             match=of.ofp_match(in_port=64,
+                                                                dl_type=pkt.ethernet.IP_TYPE,
+                                                                nw_dst=(IPAddr("130.127.3.192"), 32))))
+
+        #MU Inbound send to OSU DTN
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=20),
+                                             priority=700,
+                                             match=of.ofp_match(in_port=64, dl_vlan=1751)))
+
+        #OSU DTN traffic outbound to MU IP 128.146.162.36
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=64),
+                                             priority=700,
+                                             match=of.ofp_match(in_port=20, dl_type=pkt.ethernet.IP_TYPE)))
+
+        #OSU DTN Traffic outbound to public IPs
+        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_strip_vlan(), of.ofp_action_output(port=61)],
+                                             priority=500,
+                                             match=of.ofp_match(in_port=20,
+                                                                dl_vlan=1751,
+                                                                dl_type=pkt.ethernet.IP_TYPE,
+                                                                nw_src=(IPAddr("128.146.162.35"), 32))))
+        #OSU DTN Traffic outbound ARP flood
+        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_strip_vlan(), of.ofp_action_output(of.OFPP_FLOOD)],
+                                             priority=500,
+                                             match=of.ofp_match(in_port=20,
+                                                                dl_type=pkt.ethernet.ARP_TYPE,
+                                                                dl_src=(EthAddr("00:02:c9:1f:d1:60")))))
+
+        #OSU DTN Outbound Default to Controller
+        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_CONTROLLER),
+                                             priority=2,
+                                             match=of.ofp_match(in_port=20)))
 
         #OSU Public inbound traffic default to OSU DTN
         self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=1750),
                                                      of.ofp_action_output(port=20)],
                                              priority=1,
                                              match=of.ofp_match(in_port=61)))
-
-        #OSU DTN Outbound Default to Controller
+        #AL2S inbound default to controller
         self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_CONTROLLER),
-                                             priority=2,
-                                             match=of.ofp_match(in_port=20)))
+                                             priority=1,
+                                             match=of.ofp_match(in_port=64)))
+
+
+
 
 
 class DMZSwitch(object):
