b7f89965aa0b848dd661468cf13f9102c3b9ee85
berryman <alexbman@gmail.com>
2014-10-24 11:41:57 -0400

    remove dump

--- ext/paths.py
+++ ext/paths.py
@@ -8,8 +8,6 @@ from pox.lib.util import dpid_to_str
 from pox.lib.addresses import EthAddr
 import pox.lib.packet as pkt
 
-from pox.lib.util import dpidToStr
-
 log = core.getLogger()
 packet_id = 0
 
@@ -91,52 +89,6 @@ class DMZFlows(object):
                                              priority=1,
                                              match=of.ofp_match(in_port=64)))
 
-    log = core.getLogger()
-
-    _verbose = None
-    _max_length = None
-    _types = None
-    _show_by_default = None
-
-    def _handle_PacketIn(event):
-        packet = event.parsed
-
-        show = _show_by_default
-        p = packet
-        while p:
-            if p.__class__.__name__.lower() in _types:
-                if _show_by_default:
-                    # This packet is hidden
-                    return
-                else:
-                    # This packet should be shown
-                    show = True
-                    break
-                return
-            if not hasattr(p, 'next'): break
-            p = p.next
-
-        if not show: return
-
-        msg = dpidToStr(event.dpid) + ": "
-        msg = ""
-        if _verbose:
-            msg += packet.dump()
-        else:
-            p = packet
-            while p:
-                if isinstance(p, basestring):
-                    msg += "[%s bytes]" % (len(p),)
-                    break
-                msg += "[%s]" % (p.__class__.__name__,)
-                p = p.next
-
-        if _max_length:
-            if len(msg) > _max_length:
-                msg = msg[:_max_length - 3]
-                msg += "..."
-        core.getLogger("dump:" + dpidToStr(event.dpid)).debug(msg)
-
 
 class DMZSwitch(object):
     def __init__(self):
@@ -150,32 +102,3 @@ class DMZSwitch(object):
 def launch(verbose=False, max_length=110, full_packets=True,
            hide=False, show=False):
     core.registerNew(DMZSwitch)
-    global _verbose, _max_length, _types, _show_by_default
-    _verbose = verbose
-    _max_length = max_length
-    force_show = (show is True) or (hide is False and show is False)
-    if isinstance(hide, basestring):
-        hide = hide.replace(',', ' ').replace('|', ' ')
-        hide = set([p.lower() for p in hide.split()])
-    else:
-        hide = set()
-    if isinstance(show, basestring):
-        show = show.replace(',', ' ').replace('|', ' ')
-        show = set([p.lower() for p in show.split()])
-    else:
-        show = set()
-
-    if hide and show:
-        raise RuntimeError("Can't both show and hide packet types")
-
-    if show:
-        _types = show
-    else:
-        _types = hide
-        _show_by_default = not not hide
-    if force_show:
-        _show_by_default = force_show
-
-    if full_packets:
-    # Send full packets to controller
-        core.openflow.miss_send_len = 0xffff
