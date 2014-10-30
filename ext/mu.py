aad7250bb60bbb6a0aa51763b59837a26b1d04d9
Alex Berryman <alexbman@gmail.com>
2014-10-29 23:59:48 -0400

    match IP packets

--- ext/mu.py
+++ ext/mu.py
@@ -27,7 +27,9 @@ class DMZFlows(object):
         self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=1751),
                                                      of.ofp_action_output(port=1)],
                                              priority=900,
-                                             match=of.ofp_match(in_port=3)))
+                                             match=of.ofp_match(in_port=3,
+                                                                dl_type=pkt.ethernet.IP_TYPE,
+                                                                nw_dst="128.146.162.35/32")))
 
 
 class DMZSwitch(object):
