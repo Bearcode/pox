4c1ee695ba3913d099a2560fa53a1c0aacd88125
Alex Berryman <alexbman@gmail.com>
2014-10-30 11:46:56 -0400

    fullset

--- ext/mu.py
+++ ext/mu.py
@@ -27,13 +27,40 @@ class DMZFlows(object):
 
         #Static flows
 
-        #AL2S inbound default to controller
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=1751),
-                                                     of.ofp_action_output(port=1)],
-                                             priority=901,
-                                             match=of.ofp_match(in_port=3,
-                                                                dl_type=0x800,
-                                                                nw_dst="128.146.162.35/32")))
+        #Missouri Management MAC to Gateway
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(dl_src=EthAddr("74:8e:f8:fc:6a:00")),
+                                             priority=900,
+                                             action=[of.ofp_action_vlan_vid(vlan_vid=350),
+                                                     of.ofp_action_output(port=1)]))
+
+        #Missouri Management IP to Gateway
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(dl_type=pkt.ethernet.IP_TYPE,
+                                                                nw_srd="128.206.117.124/32"),
+                                             priority=900,
+                                             action=[of.ofp_action_vlan_vid(vlan_vid=350),
+                                                     of.ofp_action_output(port=1)]))
+
+        #DTN2 MAC to Gateway
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
+                                                                dl_src=EthAddr("00:02:c9:1f:d4:60")),
+                                             priority=800,
+                                             action=[of.ofp_action_vlan_vid(vlan_vid=350),
+                                                     of.ofp_action_output(port=1)]))
+        #DTN2 MAC Inbound
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=1,
+                                                                dl_dst=EthAddr("00:02:c9:1f:d4:60")),
+                                             priority=800,
+                                             action=[of.ofp_action_strip_vlan(),
+                                                     of.ofp_action_output(port=3)]))
+
+        #DTN2 outbound to DTN1
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
+                                                                dl_type=pkt.ethernet.IP_TYPE,
+                                                                nw_dst="128.146.162.35/32"),
+                                             priority=700,
+                                             action=[of.ofp_action_dl_addr.set_dst(EthAddr("00:02:c9:1f:d1:60")),
+                                                     of.ofp_action_vlan_vid(vlan_vid=1751),
+                                                     of.ofp_action_output(port=1)]))
 
 
 class DMZSwitch(object):
