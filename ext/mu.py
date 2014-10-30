e7f67dc7019786312b6b62953ffec59d41ee36b7
Alex Berryman <alexbman@gmail.com>
2014-10-29 23:58:06 -0400

    match port 3 only

--- ext/mu.py
+++ ext/mu.py
@@ -24,10 +24,6 @@ class DMZFlows(object):
         #Static flows
 
         #AL2S inbound default to controller
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_strip_vlan(), of.ofp_action_output(port=3)],
-                                             priority=900,
-                                             match=of.ofp_match(in_port=1)))
-        #AL2S inbound default to controller
         self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=1751),
                                                      of.ofp_action_output(port=1)],
                                              priority=900,
