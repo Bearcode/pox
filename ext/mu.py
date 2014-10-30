eba27d63190d0b5a133bb0694506252a158f16d3
Alex Berryman <alexbman@gmail.com>
2014-10-30 12:04:22 -0400

    priority swap

--- ext/mu.py
+++ ext/mu.py
@@ -36,16 +36,10 @@ class DMZFlows(object):
                                              action=[of.ofp_action_vlan_vid(vlan_vid=350),
                                                      of.ofp_action_output(port=1)]))
 
-        #DTN2 MAC to Gateway
-        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
-                                                                dl_src=EthAddr("00:02:c9:1f:d4:60")),
-                                             priority=800,
-                                             action=[of.ofp_action_vlan_vid(vlan_vid=350),
-                                                     of.ofp_action_output(port=1)]))
         #DTN2 MAC Inbound
         self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=1,
                                                                 dl_dst=EthAddr("00:02:c9:1f:d4:60")),
-                                             priority=800,
+                                             priority=900,
                                              action=[of.ofp_action_strip_vlan(),
                                                      of.ofp_action_output(port=3)]))
 
@@ -53,10 +47,19 @@ class DMZFlows(object):
         self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
                                                                 dl_type=pkt.ethernet.IP_TYPE,
                                                                 nw_dst="128.146.162.35/32"),
-                                             priority=700,
+                                             priority=800,
                                              action=[of.ofp_action_dl_addr.set_dst(EthAddr("00:02:c9:1f:d1:60")),
                                                      of.ofp_action_vlan_vid(vlan_vid=1751),
                                                      of.ofp_action_output(port=1)]))
+
+        #DTN2 MAC to Gateway
+        self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
+                                                                dl_src=EthAddr("00:02:c9:1f:d4:60")),
+                                             priority=700,
+                                             action=[of.ofp_action_vlan_vid(vlan_vid=350),
+                                                     of.ofp_action_output(port=1)]))
+
+
 
 
 class DMZSwitch(object):
