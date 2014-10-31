bb21e0b7841fdc6386e949d0b70e9f5253ca8811
Alex Berryman <alexbman@gmail.com>
2014-10-31 09:13:35 -0400

    new error comments

--- ext/mu.py
+++ ext/mu.py
@@ -24,6 +24,9 @@ class DMZFlows(object):
         #Static flows
 
         #Missouri Management MAC to Gateway
+        #Oct 31 12:47:50:D:ERROR: Ctrl. Transaction id:  6
+        # Status: FLOW_MOD ERROR: Reason: Error: Not a valid L2 or L3 or L23 Openflow rule
+
         self.connection.send(of.ofp_flow_mod(match=of.ofp_match(dl_src=EthAddr("74:8e:f8:fc:6a:00")),
                                              priority=900,
                                              action=[of.ofp_action_vlan_vid(vlan_vid=350),
@@ -37,6 +40,8 @@ class DMZFlows(object):
                                                      of.ofp_action_output(port=1)]))
 
         #DTN2 MAC Inbound
+        #Oct 31 12:47:50:D:ERROR: Ctrl. Transaction id:  8
+        # Status: FLOW_MOD ERROR: Reason: Flow record construction failed!
         self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=1,
                                                                 dl_dst=EthAddr("00:02:c9:1f:d4:60")),
                                              priority=900,
@@ -53,6 +58,8 @@ class DMZFlows(object):
                                                      of.ofp_action_output(port=1)]))
 
         #DTN2 MAC to Gateway
+        #Oct 31 12:50:10:D:ERROR: Ctrl. Transaction id:  10
+        # Status: FLOW_MOD ERROR: Reason: Flow record construction failed!
         self.connection.send(of.ofp_flow_mod(match=of.ofp_match(in_port=3,
                                                                 dl_src=EthAddr("00:02:c9:1f:d4:60")),
                                              priority=700,
