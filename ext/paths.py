a841cdb8e6082ea87414d8f38c6a8a1deb495bb0
Alex Berryman <alexbman@gmail.com>
2014-11-12 12:43:41 -0500

    Prep for api

--- ext/paths.py
+++ ext/paths.py
@@ -7,10 +7,21 @@ import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpid_to_str
 from pox.lib.addresses import EthAddr
 import pox.lib.packet as pkt
+from flask import Flask, jsonify
+import settings
 
 log = core.getLogger()
 packet_id = 0
 
+app = Flask(__name__)
+
+@app.route('/todo/api/v1.0/flows', methods=['GET'])
+def get_flows():
+    flows = []
+    for var in dir(settings):
+            if var.startswith("osu"):
+                flows.append(settings.__dict__[var])
+    return jsonify({'flows': flows})
 
 class DMZFlows(object):
     def __init__(self, connection):
@@ -22,72 +33,9 @@ class DMZFlows(object):
         connection.addListeners(self)
 
         #Static flows
-        #Drop PVST+ traffic
-        self.connection.send(of.ofp_flow_mod(action=[],
-                                             priority=9999,
-                                             match=of.ofp_match(in_port=64, dl_dst=EthAddr("01:00:0c:cc:cc:cd"))))
-
-        #Forward outbound Clemson ARP replies
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=3070),
-                                                     of.ofp_action_output(port=64)],
-                                             priority=801,
-                                             match=of.ofp_match(in_port=20,
-                                                                dl_type=pkt.ethernet.ARP_TYPE,
-                                                                dl_dst=EthAddr("00:24:38:9c:13:00"))))
-        #Forward Clemson Inbound IP
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_strip_vlan(),
-                                                     of.ofp_action_output(port=20)],
-                                             priority=800,
-                                             match=of.ofp_match(in_port=64, dl_vlan=3070)))
-
-        #Forward Clemson Outbound IP
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_vlan_vid(vlan_vid=3070),
-                                                     of.ofp_action_output(port=64)],
-                                             priority=800,
-                                             match=of.ofp_match(in_port=20,
-                                                                dl_type=pkt.ethernet.IP_TYPE,
-                                                                dl_dst=EthAddr("00:24:38:9c:13:00"))))
-
-        #MU L2 Inbound send to OSU DTN
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_strip_vlan(), of.ofp_action_output(port=20)],
-                                             priority=700,
-                                             match=of.ofp_match(in_port=64, dl_vlan=1751)))
-
-        #OSU DTN traffic outbound to MU IP 128.206.117.1 to AL2S
-        self.connection.send(of.ofp_flow_mod(action=[of.ofp_action_dl_addr.set_dst(EthAddr("00:02:C9:1F:D4:60")),
-                                                     of.ofp_action_vlan_vid(vlan_vid=1751),
-                                                     of.ofp_action_output(port=64)],
-                                             priority=700,
-                                             match=of.ofp_match(in_port=20,
-                                                                dl_type=pkt.ethernet.IP_TYPE,
-                                                                nw_dst="128.206.117.1/32")))
-
-        #OSU DTN Traffic outbound to public IPs
-        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=61),
-                                             priority=500,
-                                             match=of.ofp_match(in_port=20,
-                                                                dl_type=pkt.ethernet.IP_TYPE,
-                                                                nw_src="128.146.162.35/32")))
-        #OSU DTN Traffic outbound ARP
-        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=61),
-                                             priority=500,
-                                             match=of.ofp_match(in_port=20,
-                                                                dl_type=pkt.ethernet.ARP_TYPE,
-                                                                dl_src=(EthAddr("00:02:c9:1f:d1:60")))))
-
-        #OSU DTN Outbound Default to Controller
-        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_CONTROLLER),
-                                             priority=2,
-                                             match=of.ofp_match(in_port=20)))
-
-        #OSU Public inbound traffic default to OSU DTN
-        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=20),
-                                             priority=1,
-                                             match=of.ofp_match(in_port=61)))
-        #AL2S inbound default to controller
-        self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=of.OFPP_CONTROLLER),
-                                             priority=1,
-                                             match=of.ofp_match(in_port=64)))
+        for var in dir(settings):
+            if var.startswith("osu"):
+                print settings.__dict__[var]
 
 
 class DMZSwitch(object):
@@ -102,3 +50,4 @@ class DMZSwitch(object):
 def launch(verbose=False, max_length=110, full_packets=True,
            hide=False, show=False):
     core.registerNew(DMZSwitch)
+    app.run(debug=True)
